78ab262f1165ac4455082e48db66352e3486ba59
akildemir <34187742+akildemir@users.noreply.github.com>
2020-12-11 12:10:26 +0300

    ecdh tuple is exported

--- crypto/ecdh.go
+++ crypto/ecdh.go
@@ -1,9 +1,9 @@
 // nolint:errcheck
 package crypto
 
-type ecdhTuple struct {
-	mask   [32]byte
-	amount [32]byte
+type EcdhTuple struct {
+	Mask   [32]byte
+	Amount [32]byte
 }
 
 func ecdhHash(sharedSecret [32]byte) [32]byte {
@@ -28,12 +28,12 @@ func xor8(keyV [32]byte, keyK [32]byte) {
 	}
 }
 
-func EcdhDecode(ecdhInfo map[string][]byte, sharedSecret [32]byte) ecdhTuple {
-	var ecdhTuple ecdhTuple
+func EcdhDecode(ecdhInfo map[string][]byte, sharedSecret [32]byte) EcdhTuple {
+	var ecdhTuple EcdhTuple
 	// get the mask key
-	ecdhTuple.mask = genCommitmentMask(sharedSecret)
+	ecdhTuple.Mask = genCommitmentMask(sharedSecret)
 	// get the amount key
-	copy(ecdhTuple.amount[:], ecdhInfo["amount"])
-	xor8(ecdhTuple.amount, ecdhHash(sharedSecret))
+	copy(ecdhTuple.Amount[:], ecdhInfo["amount"])
+	xor8(ecdhTuple.Amount, ecdhHash(sharedSecret))
 	return ecdhTuple
 }
